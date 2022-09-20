4771c51aec08be87e5d1f72292b049a084042945
Tien Nam Dao <tien.dao2@tiki.vn>
2022-09-20 16:13:51 +0700

    fix: add structure + some common libs + ide config

---
+++ pages/address/[address].tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { GetStaticProps } from "next";
+import Layout from "../../components/Layout";
+
+type Props = {};
+
+const AddressDetailPage: React.FC<Props> = (props) => {
+	return <Layout>Implement Here</Layout>;
+};
+
+export default AddressDetailPage;
---
+++ pages/block/[block[.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { GetStaticProps } from "next";
+import Layout from "../../components/Layout";
+
+type Props = {};
+
+const BlockDetailPage: React.FC<Props> = (props) => {
+	return <Layout>Implement Here</Layout>;
+};
+
+export default BlockDetailPage;
