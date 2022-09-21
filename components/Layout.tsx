99f3c4cc6ea26a9c6efce0aed60a9c40398a2863
Tien Nam Dao <tien.dao2@tiki.vn>
2022-09-21 11:11:36 +0700

    merge dev

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,15 +1,18 @@
 import React, { ReactNode } from "react";
+import BackgroundCard from "./BackgroundCard";
 import Navbar from "./Navbar";
 
 type Props = {
-  children: ReactNode;
-};
+	children: ReactNode
+}
 
 const Layout: React.FC<Props> = (props) => (
   <div className="dark--mode">
     <Navbar />
     <div className="layout" style={{height: "200vh"}}>{props.children}</div>
-   
+	<BackgroundCard>
+				<h1>afsdfasdf</h1>
+			</BackgroundCard>
   </div>
 );
 
