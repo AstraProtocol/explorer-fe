d5abcd7a7ef64424c468c4b4574807cf7a4d6425
monkey <daonamtien1@gmail.com>
2022-09-26 09:21:54 +0700

    fix: merge feat/asa-163

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 
-import '@astraprotocol//astra-ui/shared/style.css'
+import '@astraprotocol/astra-ui/lib/shared/style.css'
 import store, { persistor } from '../store'
 
 const App = ({ Component, pageProps }: AppProps) => {
