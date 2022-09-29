4dd170dce4c4b8d4d4874fbb29180d0370f4b07b
monkey <daonamtien1@gmail.com>
2022-09-29 14:15:06 +0700

    feat: update display block proposer with monikier

--- view/Block/BlockBriefRow.tsx
+++ view/Block/BlockBriefRow.tsx
@@ -7,13 +7,20 @@ import { ellipseBetweenText, LinkMaker } from 'utils/helper'
 import styles from './style.module.scss'
 
 type BlockBriefRowProps = {
+	validator: ValidatorData
 	blockNumber: number
 	updatedAt: number | string
 	transactions: number
 	proposerAddress: string
 }
 
-export default function BlockBriefRow({ blockNumber, updatedAt, transactions, proposerAddress }: BlockBriefRowProps) {
+export default function BlockBriefRow({
+	validator,
+	blockNumber,
+	updatedAt,
+	transactions,
+	proposerAddress
+}: BlockBriefRowProps) {
 	return (
 		<div className={clsx(styles.rowBrief, 'padding-left-lg padding-right-lg padding-top-sm padding-bottom-sm')}>
 			<div className={clsx(styles.icon, 'margin-right-sm')}>
@@ -28,11 +35,16 @@ export default function BlockBriefRow({ blockNumber, updatedAt, transactions, pr
 					/>
 				</div>
 				<div className={clsx('block-ver-center', styles.info)}>
-					<div className="block-ver-center">
-						<span className={clsx('contrast-color-30 padding-right-2xs')}>Block Proposer</span>
-						<span className="contrast-color-70 money">{ellipseBetweenText(proposerAddress, 6, 6)}</span>
-						<DotSpace />
-						<span className="contrast-color-70">{transactions} Transactions</span>
+					<div>
+						<div className="block-ver-center">
+							<span className={clsx('contrast-color-30 padding-right-2xs')}>Block Proposer</span>
+							<span className="contrast-color-70 money">
+								{validator?.moniker || ellipseBetweenText(proposerAddress, 6, 6)}
+								{/* ({ellipseBetweenText(proposerAddress, 6, 6)}) */}
+							</span>
+							<DotSpace />
+							<span className="contrast-color-70">{transactions} Transactions</span>
+						</div>
 					</div>
 					<Timer updatedAt={updatedAt} />
 				</div>
--- view/Block/HomeBlock.tsx
+++ view/Block/HomeBlock.tsx
@@ -1,19 +1,27 @@
+import { ethToAstra } from '@astradefi/address-converter'
 import API_LIST from 'api/api_list'
 import clsx from 'clsx'
 import BackgroundCard from 'components/Card/Background/BackgroundCard'
 import Container from 'components/Container'
 import DotSpace from 'components/DotSpace'
 import RowLoader from 'components/Loader/RowLoader'
+import { getValidatorSummary } from 'slices/commonSlice'
+import { useAppSelector } from 'store/hooks'
 import useSWR from 'swr'
 import { LinkMaker } from 'utils/helper'
 import RowBrief from './BlockBriefRow'
 
 export function HomeBlock() {
+	const validatorSummary = useAppSelector(getValidatorSummary)
 	const _fetchCondition = () => {
 		return [API_LIST.ALL_BLOCKS]
 	}
+
 	const { data } = useSWR<BlockResponse>(_fetchCondition(), { refreshInterval: 2000 })
 	const blockTop10 = data?.items?.slice(0, 10)
+
+	console.log(blockTop10)
+
 	return (
 		<Container>
 			<div className="block-ver-center margin-bottom-md">
@@ -30,6 +38,9 @@ export function HomeBlock() {
 				<BackgroundCard>
 					{blockTop10?.map(item => (
 						<RowBrief
+							validator={validatorSummary.find(
+								(v: ValidatorData) => v.initialDelegatorAddress == ethToAstra(item.miner_hash)
+							)}
 							key={item.number}
 							blockNumber={item.number}
 							proposerAddress={item.miner_hash}
