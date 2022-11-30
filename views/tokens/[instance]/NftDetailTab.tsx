86d34b34a10e73ac8ca0d100f5231bcd95a11811
thinhtran3 <thinh.tran3@tiki.vn>
2022-11-30 15:24:38 +0700

    feat: move NFT properties to tab

--- views/tokens/[instance]/NftDetailTab.tsx
+++ views/tokens/[instance]/NftDetailTab.tsx
@@ -2,6 +2,7 @@ import BackgroundCard from 'components/Card/Background/BackgroundCard'
 import Tabs from 'components/Tabs/Tabs'
 import useRouterTag from 'hooks/useRouterTag'
 import NftTransferTab from './tabs/NftTransfers'
+import NftProperties from './tabs/NftProperties'
 
 interface Props {
 	token: string
@@ -18,9 +19,13 @@ const NftDetailTab = ({ token, tokenId, tokenData }: Props) => {
 				tabChange={setTag}
 				defaultTab={defaultTag}
 				classes="none"
-				tabs={[{ title: 'Token Transfers', id: 'token-transfers' }]}
+				tabs={[
+					{ title: 'Token Transfers', id: 'token-transfers' },
+					{ title: 'Properties', id: 'properties' }
+				]}
 				contents={{
-					'token-transfers': <NftTransferTab token={token} tokenId={tokenId} tokenData={tokenData} />
+					'token-transfers': <NftTransferTab token={token} tokenId={tokenId} tokenData={tokenData} />,
+					'properties': <NftProperties tokenData={tokenData} />
 				}}
 			></Tabs>
 		</BackgroundCard>
