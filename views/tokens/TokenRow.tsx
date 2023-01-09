714ea4cc98d403ade9af592750fbe1f460619dde
Tien Nam Dao <tien.dao2@tiki.vn>
2023-01-09 15:52:13 +0700

    fix: update display amount with NFT in tokens list

--- views/tokens/TokenRow.tsx
+++ views/tokens/TokenRow.tsx
@@ -1,7 +1,7 @@
 import { formatNumber } from '@astraprotocol/astra-ui'
 import clsx from 'clsx'
 import Typography from 'components/Typography'
-import { convertBalanceToView, ellipseRightText, getEnvNumber, LinkMaker } from 'utils/helper'
+import { convertBalanceToView, ellipseRightText, getEnvNumber, isERC721, LinkMaker } from 'utils/helper'
 import styles from './style.module.scss'
 
 type Props = {
@@ -11,6 +11,7 @@ type Props = {
 }
 
 export default function TokenRow({ index, token }: Props) {
+	const isNFT = isERC721(token.type)
 	return (
 		<div
 			className={clsx(
@@ -43,10 +44,12 @@ export default function TokenRow({ index, token }: Props) {
 
 			<div className={clsx(styles.borderLeft, styles.colTotalSupply, 'col padding-left-lg col-3')}>
 				<span className={clsx('money money-sm money-bold padding-right-xs')}>
-					{formatNumber(
-						convertBalanceToView(token.totalSupply, parseInt(token.decimals || '1')),
-						getEnvNumber('NEXT_PUBLIC_MAXIMUM_FRACTION_DIGITS')
-					)}
+					{isNFT
+						? 1
+						: formatNumber(
+								convertBalanceToView(token.totalSupply, parseInt(token.decimals || '1')),
+								getEnvNumber('NEXT_PUBLIC_MAXIMUM_FRACTION_DIGITS')
+						  )}
 				</span>
 				<span className={clsx(styles.currency, 'money money-sm money-bold')}>{token.symbol}</span>
 			</div>
