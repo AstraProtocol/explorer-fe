bd374da2daed122309f98844116bde65f57cfbed
Tien Nam Dao <tien.dao2@tiki.vn>
2023-03-02 15:16:14 +0700

    test: init

--- cypress/cypress.config.ts
+++ cypress/cypress.config.ts
@@ -1,9 +1,15 @@
-import { loadEnvConfig } from '@next/env'
 import { defineConfig } from 'cypress'
 
-const { combinedEnv } = loadEnvConfig(process.cwd())
+const { combinedEnv } : loadEnvConfig(process.cwd())
 export default defineConfig({
-	env: combinedEnv,
+	env: {...combinedEnv, 
+		NEXT_PUBLIC_COSMOS_API:'https://chainindexing.astranaut.io',
+		NEXT_PUBLIC_EVM_API:'https://blockscout.astranaut.io',
+		NEXT_PUBLIC_URL:'https://explorer.astranaut.io',
+		NEXT_PUBLIC_CHAIN_ID:11110,
+		NEXT_PUBLIC_TITLE:'Astra Explorer',
+		NEXT_PUBLIC_ENV:"mainnet"
+	},
 	e2e: {
 		baseUrl: 'http://localhost:3000',
 		retries: {
--- cypress/e2e/main.cy.ts
+++ cypress/e2e/main.cy.ts
@@ -1,6 +1,10 @@
 describe('Home Page', () => {
-	beforeEach(() => cy.visit('http://localhost:3000'))
-	it('loads home page', () => {
+	beforeEach(() => {
+		cy.viewport('macbook-13')
+		cy.visit('http://localhost:3000')
+	})
+	it('loads home page with right title/chain', () => {
 		cy.title().should('eq', 'Astra Explorer Testnet')
+		cy.get('.menu-chain').should('eq', 'Astra Mainnet')
 	})
 })
