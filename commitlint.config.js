8792b39654e97b7a98c30d4316e813ffeec59c17
tiendn <tiendn@stellalab.com>
2023-03-16 11:08:32 +0700

    ci: update commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -12,7 +12,9 @@ module.exports = {
 	extends: ['@commitlint/config-conventional'],
 	rules: {
 		'body-leading-blank': [1, 'always'],
+		'body-max-line-length': [2, 'never'],
 		'footer-leading-blank': [1, 'always'],
+		'footer-max-line-length': [2, 'never'],
 		'header-max-length': [2, 'always', 100],
 		'scope-case': [2, 'always', 'lower-case'],
 		'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
