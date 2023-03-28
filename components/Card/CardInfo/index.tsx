070f41fe4803e2c78e731d8a2d5eaa1898357b7d
tiendn <tiendn@stellalab.com>
2023-03-28 15:40:51 +0700

    refactor: split component

--- components/Card/CardInfo/index.tsx
+++ components/Card/CardInfo/index.tsx
@@ -1,10 +1,4 @@
-import {
-	CryptoIcon,
-	ellipseBetweenText,
-	Table,
-	Typography as TypographyUI,
-	useMobileLayout
-} from '@astraprotocol/astra-ui'
+import { CryptoIcon, Table, Typography as TypographyUI, useMobileLayout } from '@astraprotocol/astra-ui'
 import { CryptoIconNames } from '@astraprotocol/astra-ui/lib/es/components/CryptoIcon'
 import { Column, RowData } from '@astraprotocol/astra-ui/lib/es/components/Table/Table'
 import clsx from 'clsx'
@@ -12,11 +6,11 @@ import CopyButton from 'components/Button/CopyButton'
 import Tag from 'components/Tag/PolygonTag'
 import Typography from 'components/Typography'
 import { LabelBackgroundTypes, LabelTypes } from 'components/Typography/Label'
-import Image from 'next/image'
 import { TransactionCardTypeEnum } from 'utils/enum'
 import { ellipseRightText } from 'utils/helper'
 import BackgroundCard from '../Background/BackgroundCard'
 import CardTabs from './Components/CardTabs'
+import ContractTransferInternal from './Components/ContractTransferInternal'
 import Decode, { DecodeProps } from './Components/Decode'
 import RawInput from './Components/RawInput'
 import Transfers from './Components/Transfers'
@@ -178,43 +172,7 @@ export default function CardInfo({
 										</div>
 									) : null}
 									{type === TransactionCardTypeEnum.INTERACT_CONTRACT_WITH_TRANSFER_INTERNAL ? (
-										<div className="col">
-											<div className="row">
-												<Typography.LinkText href={content.link || ''}>
-													<span className="text text-base icon-contract contrast-color-70 margin-right-2xs" />
-													{content.text || content.value}
-												</Typography.LinkText>
-												<CopyButton textCopy={content.value as string} />
-											</div>
-											{content.internalTransfer?.map((t: InternalTransferContent) => (
-												<div key={t.index} className="margin-left-sm margin-top-xs">
-													<Image
-														className="contrast-color-100"
-														alt="internal-tx"
-														src={'/images/icons/shape.svg'}
-														width={8}
-														height={8}
-													/>
-													<span className="margin-left-xs text text-sm contrast-color-70">
-														Transfer <span className="money money-sm">{t.value} ASA</span>
-														{'  '}
-														From{'  '}
-														<Typography.LinkText href={t.from || ''}>
-															{t.fromText
-																? `${t.fromText} (${ellipseBetweenText(t.from)})`
-																: ellipseBetweenText(t.from)}
-														</Typography.LinkText>
-														{'  '}
-														To{'  '}
-														<Typography.LinkText href={t.to || ''}>
-															{t.toText
-																? `${t.toText} (${ellipseBetweenText(t.to)})`
-																: ellipseBetweenText(t.to)}
-														</Typography.LinkText>
-													</span>
-												</div>
-											))}
-										</div>
+										<ContractTransferInternal content={content} />
 									) : null}
 									{type === TransactionCardTypeEnum.LABEL ? (
 										<div className="block-center margin-right-md">
