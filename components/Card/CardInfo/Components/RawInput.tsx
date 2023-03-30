782b788d5fcd8f0206f032cb5c80cbab9f9fa4d5
tiendn <tiendn@stellalab.com>
2023-03-30 13:51:24 +0700

    fix: just display HEX tab when revertReason is UTF8

--- components/Card/CardInfo/Components/RawInput.tsx
+++ components/Card/CardInfo/Components/RawInput.tsx
@@ -23,18 +23,22 @@ const Copy = ({ text }) => {
 
 export default function RawInput({ text }: RawInputProps) {
 	let utf8Text = text
+	const regex = /0[xX][0-9a-fA-F]+/g
+	const isHex = !!utf8Text.match(regex)
 	try {
 		utf8Text = web3.utils.toAscii(text)
 	} catch (e) {
 		Sentry.captureException(e)
 	}
+
+	const tabs = []
+	if (isHex) tabs.push({ title: 'Hex', id: 'hex', padding: ' ' })
+	tabs.push({ title: 'UTF-8', id: 'utf8', padding: ' ' })
+
 	return (
 		<div style={{ maxWidth: '885px', maxHeight: '200px', overflowY: 'auto' }}>
 			<Tabs
-				tabs={[
-					{ title: 'Hex', id: 'hex', padding: ' ' },
-					{ title: 'UTF-8', id: 'utf8', padding: ' ' }
-				]}
+				tabs={tabs}
 				contents={{
 					hex: <Copy text={text} />,
 					utf8: <Copy text={utf8Text} />
