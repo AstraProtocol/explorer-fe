24da5ce8462cad39cd9b9107b5cb2520e0ab3e0f
tiendn <tiendn@stellalab.com>
2023-05-19 11:34:10 +0700

    refactor: missing remove code

--- views/tokens/TokenOverview.tsx
+++ views/tokens/TokenOverview.tsx
@@ -44,7 +44,7 @@ const TokenOverview = ({ token, tokenData }: Props) => {
 							tokenData.totalSupply
 								? isNFT
 									? tokenData.totalSupply
-									: convertBalanceToView(tokenData.totalSupply, parseInt(tokenData.decimals || '1'))
+									: convertBalanceToView(tokenData.totalSupply, tokenData.decimals || '1')
 								: ''
 						}
 						fixNumber={5}
