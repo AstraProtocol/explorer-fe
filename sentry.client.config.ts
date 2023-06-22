9e1f28d1e12458fe1c7f4c37b6d606c983ba75e7
tiendn <tiendn@stellalab.com>
2023-06-22 23:52:08 +0700

    chore: add sentry trace rate

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -18,7 +18,7 @@ Sentry.init({
 
 	environment: ENV,
 	// Adjust this value in production, or use tracesSampler for greater control
-	tracesSampleRate: 0,
+	tracesSampleRate: 1.0,
 	// ...
 	// Note: if you want to override the automatic release value, do not set a
 	// `release` value here - use the environment variable `SENTRY_RELEASE`, so
