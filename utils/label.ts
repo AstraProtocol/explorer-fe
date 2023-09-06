ed0c6484ec8af1b3804a1007ef48cb6a22c8d063
tiendn <tiendn@stellalab.com>
2023-09-06 15:12:24 +0700

    fix: update getAddressLabel in some cases and write tests

--- utils/label.ts
+++ utils/label.ts
@@ -3,8 +3,10 @@ import { isMainnet } from './helper'
 const addressLabelArr = isMainnet ? require('../data/address/mainnet.json') : require('../data/address/testnet.json')
 
 export const getAddressLabel = (label, address: string) => {
-	if (label && label.trim() !== '') return label + ' ✅'
-	if (!address) return ''
-	if (addressLabelArr[address.toLowerCase()]) return addressLabelArr[address.toLowerCase()] + ' ✅'
-	return ''
+	if (!address) return label || ''
+
+	const whitelistLabel = addressLabelArr[address.toLowerCase()]
+	if (whitelistLabel) return whitelistLabel + ' ✅'
+
+	return label || ''
 }
